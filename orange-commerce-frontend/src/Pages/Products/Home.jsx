4ad91c01cbf74192ca7a0a46ee581620982062f4
piouskenny <kehindeu13@gmail.com>
2023-06-26 19:00:16 +0100

    beautified Card Component

--- orange-commerce-frontend/src/Pages/Products/Home.jsx
+++ orange-commerce-frontend/src/Pages/Products/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../../components/Hero/Hero";
 import Categories from "../../components/Categories/Categories";
+import Cards from "../../components/Cards/Cards";
 
 const Home = () => {
   return (
     <body>
       <Hero />
       <Categories />
+      <Cards />
     </body>
   );
 };
--- orange-commerce-frontend/src/components/Cards/Cards.jsx
+++ orange-commerce-frontend/src/components/Cards/Cards.jsx
@@ -1,41 +1,46 @@
 import React from "react";
 import SingleCards from "./SingleCards";
-import Categories from "./Categories/Categories";
+
 const CardContent = [
   {
     title: "Blue Nike Show",
     Category: "Shoe",
     Image: "",
+    Price: "$800",
     Description: "Lorem Ipsoum tresus Mandes",
   },
   {
     title: "White Addidas Jogger",
     Category: "Cloth",
     Image: "",
+    Price: "$300",
     Description: "Lorem Ipsoum tresus Mandes",
   },
   {
     title: "CK Blue business",
     Category: "Wristwatch",
     Image: "",
+    Price: "$600",
     Description: "Lorem Ipsoum tresus Mandes",
   },
   {
     title: "Gucci Bag",
     Category: "Bag",
     Image: "",
+    Price: "$1000",
     Description: "Lorem Ipsoum tresus Mandes",
   },
 ];
 
 const Cards = () => {
   return (
-    <div>
+    <div className="lg:flex justify-center items-center content-center mt-10 space-y-5">
       {CardContent.map((content) => (
         <SingleCards
           Image={content.Image}
           title={content.title}
           Category={content.Category}
+          Price={content.Price}
           Description={content.Description}
         />
       ))}
