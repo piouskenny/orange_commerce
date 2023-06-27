7e1319e5e80290b767c7e390ad34109d9eaf3788
piouskenny <kehindeu13@gmail.com>
2023-06-27 10:53:35 +0100

    created Products Controller class

---
+++ orange-commerce-backend/app/controllers/ProductsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\controllers;
+
+class ProductsController
+{
+    public function all_products()
+    {
+    }
+
+    public function add_products()
+    {
+    }
+
+    public function delete_products()
+    {
+    }
+}
